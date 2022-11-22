e13e924b510747a235982bfc3026fe5fe8609314
Andrew Fawcett <andy@andyinthecloud.com>
2022-11-21 16:40:34 -0800

    Add initial oAuth implementation

--- source/formulaforceweb/src/server/server.js
+++ source/formulaforceweb/src/server/server.js
@@ -1,8 +1,20 @@
 import { createServer } from "lwr";
+import session from "express-session"
+import jsforce from "jsforce"
+import dotenv from "dotenv";
 import RaceService from "./races.js";
 
-const PORT = parseInt(process.env.PORT || "3001", 10);
+// Load .env configuration file
+dotenv.config();
+const PORT = parseInt(process.env.PORT || "3000", 10);
 const SERVER_MODE = "development" === process.env.NODE_ENV ? "dev" : "prod-compat";
+const {
+    SALESFORCE_LOGIN_DOMAIN,
+    SALESFORCE_CLIENT_ID,
+    SALESFORCE_CLIENT_SECRET,
+    SALESFORCE_CALLBACK_URL,
+    NODE_SESSION_SECRET_KEY
+} = process.env;
 
 // Create the LWR App Server
 const lwrServer = createServer({
@@ -10,18 +22,59 @@ const lwrServer = createServer({
     port: PORT,
 });
 
-// Get the internal express app
+// Get the internal Express server from within the LWR Server
 const expressApp = lwrServer.getInternalServer("express");
+  
+//
+// Configure express-session to store Session Id and Instance URL
+//
+expressApp.use(
+    session({ 
+        secret: NODE_SESSION_SECRET_KEY, 
+        cookie: { secure: 'auto' },
+        resave: false, 
+        saveUninitialized: false}))
 
-// Add API's
+//
+// OAuth2 client information can be shared with multiple connections.
+//
+var oauth2 = new jsforce.OAuth2({
+  loginUrl : SALESFORCE_LOGIN_DOMAIN,
+  clientId : SALESFORCE_CLIENT_ID,
+  clientSecret : SALESFORCE_CLIENT_SECRET,
+  redirectUri : SALESFORCE_CALLBACK_URL
+});
+
+//
+// Get authorization url and redirect to it.
+//
+expressApp.get('/oauth2/auth', function(req, res) {
+  res.redirect(oauth2.getAuthorizationUrl({ scope : 'api id web refresh_token' }));
+});
+
+//
+// Handle oAuth callback and extract and store session token
+//
+expressApp.get('/oauth2/callback', function(req, res) {
+    var conn = new jsforce.Connection({ oauth2 : oauth2 });
+    var code = req.param('code');
+    conn.authorize(code, function(err, userInfo) {
+      if (err) { return console.error(err); }
+      req.session.accessToken = conn.accessToken;
+      req.session.instanceUrl = conn.instanceUrl;
+      // Redirect to main page
+      res.redirect('/');
+    });
+});
+
+// Add FormulaForce API's
 const raceService = new RaceService();
 expressApp.get('/api/races', function(req, res) { raceService.getRaces(req, res); });
-  
+
 // Start the server
 lwrServer
     .listen(({ port, serverMode }) => {
         console.log(`✅ App listening on port ${port} in ${serverMode} mode!`);
-        console.log(`Url http://localhost:${port}`);
     })
     .catch((err) => {
         console.error(err);
