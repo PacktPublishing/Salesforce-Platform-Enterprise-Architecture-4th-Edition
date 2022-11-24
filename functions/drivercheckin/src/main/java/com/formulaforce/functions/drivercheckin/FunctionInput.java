e9e058036f4b9fd529ca45f061e4558ed613f8a9
Andrew Fawcett <andy@andyinthecloud.com>
2022-11-23 16:57:57 -0800

    Added Driver Checkin Function

---
+++ functions/drivercheckin/src/main/java/com/formulaforce/functions/drivercheckin/FunctionInput.java
@@ -0,0 +1,13 @@
+package com.formulaforce.functions.drivercheckin;
+
+public class FunctionInput {
+  private String raceId;
+  private String driverId;
+
+  public String getRaceId() {
+    return raceId;
+  }
+  public String getDriverId() {
+    return driverId;
+  }
+}
---
+++ functions/drivercheckin/src/main/java/com/formulaforce/functions/drivercheckin/FunctionOutput.java
@@ -0,0 +1,9 @@
+package com.formulaforce.functions.drivercheckin;
+
+public class FunctionOutput {
+  public final String qrCodeBase64;
+
+  public FunctionOutput(String qrCodeBase64) {
+    this.qrCodeBase64 = qrCodeBase64;
+  }
+}
