34b94d9ff2f08d9dbd52700f0594902a21307cfe
Andrew Fawcett <andy@andyinthecloud.com>
2022-12-04 11:25:11 -0800

    Clear out old versions of chapter 8 Heroku apps

--- source/formulaforceweb/scripts/copy-slds.mjs
+++
@@ -1,6 +0,0 @@
-// An example script to copy the SLDS resources into your app
-// my-app/scripts/copy-slds.mjs
-import cpx from 'cpx';
-cpx.copy('./node_modules/@salesforce-ux/design-system/assets/**/*', 'src/assets', () => {
-    console.log('Done copying SLDS resources');
-});
--- source/formulaforceweb/src/server/races.js
+++
@@ -1,5 +0,0 @@
-export default class RaceService {
-   getRaces(req, res) {
-      res.send('{ "summary": "Race Results" }');
-   }
-}
