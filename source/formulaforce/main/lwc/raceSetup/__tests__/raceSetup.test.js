c7ec450af5ad2cee5c1286b914eb4df84912112c
Andrew Fawcett <andy@andyinthecloud.com>
2023-01-08 17:33:21 -0800

    Fixed tests

--- source/formulaforce/main/lwc/raceSetup/__tests__/raceSetup.test.js
+++ source/formulaforce/main/lwc/raceSetup/__tests__/raceSetup.test.js
@@ -1,13 +1,27 @@
 // raceSetup.test.js
 import { createElement } from 'lwc';
 import RaceSetup from 'c/raceSetup';
-import { createApexTestWireAdapter } from '@salesforce/sfdx-lwc-jest';
 import getDriverList from '@salesforce/apex/RaceSetupComponentController.getDriverList';
 import addDrivers from '@salesforce/apex/RaceSetupComponentController.addDriversLwc';
+import { CloseActionScreenEventName } from 'lightning/actions';
 import { ShowToastEventName } from 'lightning/platformShowToastEvent';
 
 // Mock list of Drivers
 const mockGetDriverList = require('./data/getDriverList.json');
+
+// Mock getDriverList wire adapter
+jest.mock(
+    '@salesforce/apex/RaceSetupComponentController.getDriverList',
+    () => {
+        const {
+            createApexTestWireAdapter
+        } = require('@salesforce/sfdx-lwc-jest');
+        return {
+            default: createApexTestWireAdapter(jest.fn())
+        };        
+    },
+    { virtual: true }
+);
 
 // Mocking imperative Apex method call
 jest.mock(
@@ -20,27 +34,25 @@ jest.mock(
     { virtual: true }
 );
 
-// Register as Apex wire adapter. Some tests verify that provisioned values trigger desired behavior.
-const getDriverListAdapter = createApexTestWireAdapter(getDriverList);
-
 describe('c-raceSetup', () => {
-
-    // The jsdom instance is shared across test cases in a single file so reset the DOM
+    
     afterEach(() => {
+        // The jsdom instance is shared across test cases in a single file so reset the DOM
         while (document.body.firstChild) {
             document.body.removeChild(document.body.firstChild);
         }
+        // Prevent data saved on mocks from leaking between tests
+        jest.clearAllMocks();        
     });
 
     // Helper function to wait until the microtask queue is empty. 
     // This is needed for promise timing when calling imperative Apex.
-    function flushPromises() {
-        // eslint-disable-next-line no-undef
-        return new Promise(resolve => setImmediate(resolve));
+    async function flushPromises() {
+        return Promise.resolve();
     }
 
     // Test: displays drivers
-    it('displays drivers', () => {
+    it('displays drivers', async () => {
 
         // Given
         const element = createElement('c-raceSetup', {
@@ -49,17 +61,16 @@ describe('c-raceSetup', () => {
 
         // Then
         document.body.appendChild(element);
-        getDriverListAdapter.emit(mockGetDriverList);
-
+        getDriverList.emit(mockGetDriverList);
+        
         // When 
-        return Promise.resolve().then(() => {
-            const tableEl = element.shadowRoot.querySelector('lightning-datatable');
-            expect(tableEl.data.length).toBe(mockGetDriverList.length);
-        });
+        await flushPromises();
+        const tableEl = element.shadowRoot.querySelector('lightning-datatable');
+        expect(tableEl.data.length).toBe(mockGetDriverList.length);
     });
     
     // Test: add drivers success
-    it('add drivers success', () => {
+    it('add drivers success', async () => {
 
         // Given
         const MOCK_APEX_RESPONSE = 2;
@@ -85,33 +96,34 @@ describe('c-raceSetup', () => {
                 "driversToAdd" : [DRIVER1_ID, DRIVER2_ID]
             };        
         const showToastHandler = jest.fn();
-        const addedHandler = jest.fn();
+        const closeActionScreenHandler = jest.fn();
         const element = createElement('c-raceSetup', {
             is: RaceSetup
         });
-        element.raceId = RACE_ID;
+        element.recordId = RACE_ID;
         element.addEventListener(ShowToastEventName, showToastHandler);
-        element.addEventListener('added', addedHandler);
-        addDrivers.mockResolvedValue(MOCK_APEX_RESPONSE);
+        element.addEventListener(CloseActionScreenEventName, closeActionScreenHandler);        
+        addDrivers.mockResolvedValue(MOCK_APEX_RESPONSE);        
 
         // Then
         document.body.appendChild(element);
-        const tableEl = element.shadowRoot.querySelector('lightning-datatable');            
+        getDriverList.emit(mockGetDriverList);
+        await flushPromises();
+        const tableEl = element.shadowRoot.querySelector('lightning-datatable');
         tableEl.getSelectedRows = jest.fn().mockReturnValue(SELECTED_ROWS);
 
         // When 
-        const buttonEl = element.shadowRoot.querySelectorAll('lightning-button')[1];
+        const buttonEl = element.shadowRoot.querySelector('lightning-button[data-id=addDriversButton]');
         buttonEl.click();
-        return flushPromises().then(() => {
-            expect(addDrivers.mock.calls[0][0]).toEqual(EXPECTED_APEX_PARAMETERS);
-            expect(addedHandler).toHaveBeenCalled();
-            expect(showToastHandler).toHaveBeenCalled();
-            expect(showToastHandler.mock.calls[0][0].detail.message).toBe('Add ' + MOCK_APEX_RESPONSE + ' drivers.');
-        });
+        await flushPromises();
+        expect(addDrivers.mock.calls[0][0]).toEqual(EXPECTED_APEX_PARAMETERS);
+        expect(showToastHandler).toHaveBeenCalled();
+        expect(showToastHandler.mock.calls[0][0].detail.message).toBe('Add ' + MOCK_APEX_RESPONSE + ' drivers.');
+        expect(closeActionScreenHandler).toHaveBeenCalled();
     });
 
     // Test: add drivers fail
-    it('add drivers fail', () => {
+    it('add drivers fail', async () => {
 
         // Given
         const MOCK_APEX_RESPONSE = {
@@ -142,15 +154,16 @@ describe('c-raceSetup', () => {
 
         // Then
         document.body.appendChild(element);
+        getDriverList.emit(mockGetDriverList);
+        await flushPromises(); 
         const tableEl = element.shadowRoot.querySelector('lightning-datatable');            
         tableEl.getSelectedRows = jest.fn().mockReturnValue(SELECTED_ROWS);
 
         // When 
         const buttonEl = element.shadowRoot.querySelectorAll('lightning-button')[1];
         buttonEl.click();
-        return flushPromises().then(() => {
-            expect(showToastHandler).toHaveBeenCalled();
-            expect(showToastHandler.mock.calls[0][0].detail.message).toBe('Contestants can only be added to scheduled races');
-        });
+        await flushPromises();
+        expect(showToastHandler).toHaveBeenCalled();
+        expect(showToastHandler.mock.calls[0][0].detail.message).toBe('Contestants can only be added to scheduled races');
     });    
 });
--- source/formulaforce/test/jest-mocks/lightning/actions.js
+++ source/formulaforce/test/jest-mocks/lightning/actions.js
@@ -1,4 +1,10 @@
+/**
+ * Close Screen Action Event base class
+ */
+export const CloseActionScreenEventName = 'lightning__actionsclosescreen';
+
 export class CloseActionScreenEvent extends CustomEvent {
     constructor() {
+        super(CloseActionScreenEventName, { bubbles: false, composed: false });
     }
 }
