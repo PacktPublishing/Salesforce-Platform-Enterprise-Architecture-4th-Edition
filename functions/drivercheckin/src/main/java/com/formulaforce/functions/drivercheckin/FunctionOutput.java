bf3ffef605a12455fb7bb09dd6793323044fc5ad
srishhty <101326467+srishhty@users.noreply.github.com>
2023-03-23 04:16:50 +0530

    Delete functions/drivercheckin directory

--- functions/drivercheckin/src/main/java/com/formulaforce/functions/drivercheckin/FunctionOutput.java
+++
@@ -1,9 +0,0 @@
-package com.formulaforce.functions.drivercheckin;
-
-public class FunctionOutput {
-  public final String qrCodeBase64;
-
-  public FunctionOutput(String qrCodeBase64) {
-    this.qrCodeBase64 = qrCodeBase64;
-  }
-}
